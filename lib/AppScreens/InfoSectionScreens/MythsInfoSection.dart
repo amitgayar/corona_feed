95ff88a43dbd86b6f771de1c4c600933a2ff0e47
= <=>
2020-04-08 18:17:49 +0530

    Myth section fix

--- lib/AppScreens/InfoSectionScreens/MythsInfoSection.dart
+++ lib/AppScreens/InfoSectionScreens/MythsInfoSection.dart
@@ -1,57 +1,85 @@
 import 'package:flutter/material.dart';
 import 'package:modular_login/constants/constants.dart';
 
-class MythsInfoSection extends StatelessWidget {
+class MythsInfoSection extends StatefulWidget {
+  @override
+  _MythsInfoSectionState createState() => _MythsInfoSectionState();
+}
+
+class _MythsInfoSectionState extends State<MythsInfoSection> {
+
+  bool showAll ;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    showAll = false;
+  }
+
   @override
   Widget build(BuildContext context) {
     return
       Column(
         children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.fromLTRB(10,10,10, 0),
-            child: ListTile(
-                title: Text(mythItemList[0]['myth'],
-                    style: TextStyle(fontWeight: FontWeight.w500)),
-                leading: ClipRect(child: Image.asset(mythItemList[0]['image']))
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(10,10,10, 0),
-            child: ListTile(
-                title: Text(mythItemList[1]['myth'],
-                    style: TextStyle(fontWeight: FontWeight.w500)),
-                leading: ClipRect(child: Image.asset(mythItemList[1]['image']))
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.fromLTRB(10,10,10, 0),
-            child: ListTile(
-                title: Text(mythItemList[2]['myth'],
-                    style: TextStyle(fontWeight: FontWeight.w500)),
-                leading: ClipRect(child: Image.asset(mythItemList[2]['image']))
-            ),
-          ),
-          ExpansionTile(
-            title: Text(""),
-            children: <Widget>[
-              ListView.builder(
-                  shrinkWrap: true,
-                  physics: ClampingScrollPhysics(),
-                  itemCount: mythItemList.length - 3,
-                  itemBuilder: (BuildContext context, int index) {
-                    final item = mythItemList[index + 3];
-                    return Padding(
-                      padding: const EdgeInsets.fromLTRB(10,10,10, 0),
-                      child: ListTile(
+//          Padding(
+//            padding: const EdgeInsets.fromLTRB(10,10,10, 0),
+//            child: ListTile(
+//                title: Text(mythItemList[0]['myth'],
+//                    style: TextStyle(fontWeight: FontWeight.w500)),
+//                leading: ClipRect(child: Image.asset(mythItemList[0]['image']))
+//            ),
+//          ),
+//          Padding(
+//            padding: const EdgeInsets.fromLTRB(10,10,10, 0),
+//            child: ListTile(
+//                title: Text(mythItemList[1]['myth'],
+//                    style: TextStyle(fontWeight: FontWeight.w500)),
+//                leading: ClipRect(child: Image.asset(mythItemList[1]['image']))
+//            ),
+//          ),
+//          Padding(
+//            padding: const EdgeInsets.fromLTRB(10,10,10, 0),
+//            child: ListTile(
+//                title: Text(mythItemList[2]['myth'],
+//                    style: TextStyle(fontWeight: FontWeight.w500)),
+//                leading: ClipRect(child: Image.asset(mythItemList[2]['image']))
+//            ),
+//          ),
+          ListView.builder(
+              shrinkWrap: true,
+              physics: ClampingScrollPhysics(),
+              itemCount: (showAll) ? mythItemList.length : 3,
+              itemBuilder: (BuildContext context, int index) {
+                final item = mythItemList[index];
+                return Padding(
+                  padding: const EdgeInsets.fromLTRB(10,10,10, 0),
+                  child: Column(
+                    children: <Widget>[
+                      ListTile(
                           title: Text(item['myth'],
                               style: TextStyle(fontWeight: FontWeight.w500)),
                           leading: ClipRect(child: Image.asset(item['image']))
                       ),
-                    );
-                  }
-              ),
-            ],
+                    Divider(),
+                    ],
+                  ),
+                );
+              }
           ),
+          Center(
+            child: ListTile(
+              title: Text((showAll) ?  "Show Less" : "Show More",
+                    textAlign: TextAlign.center,
+                    style: TextStyle(fontWeight: FontWeight.w500,color: baseColor)),
+              trailing : (showAll) ?  Icon(Icons.keyboard_arrow_up) : Icon(Icons.keyboard_arrow_down),
+              onTap: () {
+                if(showAll) showAll = false;
+                else showAll = true;
+                setState((){});
+              },
+            ),
+          )
         ],
       );
   }
